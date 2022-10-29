6c48812199f5c161f504cb82db1566cca91a6905
Denis Pereira <denispereiradosreis@gmail.com>
2022-10-29 15:16:05 -0300

    Gerando migration

--- AmigoOculto/Models/User/SugestoesPresente.cs
+++ AmigoOculto/Models/User/SugestoesPresente.cs
@@ -2,7 +2,7 @@
 {
     public class SugestoesPresente
     {
-        public int SugestaoId { get; set; }
+        public int Id { get; set; }
         public string Descricao { get; set; }
     }
 }
--- AmigoOculto/Models/User/User.cs
+++ AmigoOculto/Models/User/User.cs
@@ -2,7 +2,7 @@
 {
     public class User
     {
-        public int NomeId { get; set; }
+        public int Id { get; set; }
         public string Nome { get; set; }
         public int SugestaoId { get; set; }
         public string Senha { get; set; }
